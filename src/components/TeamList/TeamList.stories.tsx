41ee4ec4c08551695d95934057e5134abd7f7458
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-08-29 18:58:32 +1200

    more ts fixes

--- src/components/TeamList/TeamList.stories.tsx
+++ src/components/TeamList/TeamList.stories.tsx
@@ -15,13 +15,10 @@ const teams: Team[] = [
             { _id: '5f40a6e67461094ee28c02c9', title: 'Test Congregation', color: '#695e00', bgColor: '#faff00' },
             { _id: '5f49843dc2c9c8575e39550a', title: 'Test | My Group' },
         ],
-        slug: 'test---my-group-kzshi-9py',
         _type: 'team',
         contacts: [
             {
                 _id: '5f49840b0c835c5363b5930b',
-                status: 'active',
-                keywords: ['test congregation'],
                 realms: [
                     {
                         _id: '5f40a6e67461094ee28c02c9',
@@ -31,14 +28,10 @@ const teams: Team[] = [
                     },
                     { _id: '5f49843dc2c9c8575e39550a', title: 'Test | My Group' },
                 ],
-                tags: [],
                 _type: 'contact',
                 firstName: 'Test',
                 title: 'Test Robin',
                 lastName: 'Robin',
-                gender: 'male',
-                created: '2020-08-28T22:24:11.830Z',
-                updated: '2020-08-28T22:28:10.492Z',
             },
         ],
     },
